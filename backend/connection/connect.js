bb0cb1fc0c8df315bb1de0efc8087968c5bf1a93
anshulgusain <anshulgusain99@gmail.com>
2024-03-16 16:37:53 +0530

    done

--- backend/connection/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose")
-
-const connection=mongoose.connect("mongodb+srv://anshulgusain99:twRvaJ8MCWI10jMg@cluster0.vwiwavz.mongodb.net/foodapp")
-
-module.exports={
-    connection
-}
